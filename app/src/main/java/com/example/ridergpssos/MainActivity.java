b0af490abf4b540dfdbaa46bff2985d850af39b2
kharatsonu358 <99github98>
2021-03-01 12:48:36 +0530

    1

--- app/src/main/java/com/example/ridergpssos/MainActivity.java
+++ app/src/main/java/com/example/ridergpssos/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.ridergpssos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar myToolbar;
+    private MenuItem mcn;
+
+
+
+
+
 
 
     @Override
@@ -17,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         myToolbar=findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+
     }
 
     @Override
@@ -24,5 +34,30 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater=getMenuInflater();
         inflater.inflate(R.menu.main_menu,menu);
         return true;
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_mcn:
+                openSetting();
+                return true;
+            case R.id.menu_feedback:
+                OpenFeedBack();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void OpenFeedBack() {
+        Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+        startActivity(intent);
+    }
+
+    private void openSetting() {
+        Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/ridergpssos/SettingActivity.java
+++ app/src/main/java/com/example/ridergpssos/SettingActivity.java
@@ -1,6 +1,9 @@
 package com.example.ridergpssos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,18 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        ImageButton IBBack;
+
+        IBBack=(ImageButton)findViewById(R.id.imgBtnSetting);
+        IBBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
